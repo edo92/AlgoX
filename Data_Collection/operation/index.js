7937ca7bb4b68e570a05ed73e9f28d26304f29dd
Eduard Jacobs <edoter@gmail.com>
2020-04-06 12:43:46 -0700

    ML setup

--- Data_Collection/operation/index.js
+++
@@ -1,6 +0,0 @@
-const stageOne = require('./stageOne');
-const stageTwo = require('./stageTwo');
-const stageThree = require('./stageThree');
-const stageFour = require('./stageFour');
-
-module.exports = { ...stageOne, ...stageTwo, ...stageThree, ...stageFour };
