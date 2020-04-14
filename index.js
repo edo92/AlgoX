5ccbfde9ee09e0541acf937df591a1edeb327687
Eduard Jacobs <edoter@gmail.com>
2020-04-14 04:21:09 -0700

    setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const Combinatorics = require('js-combinatorics');
+
+class Combination {
+    constructor() {
+        this.budget = 50000;
+        this.cardSize = 6;
+    }
+
+    generateCombination = () => {
+        
+    }
+}
+
+module.exports = new Combination;
